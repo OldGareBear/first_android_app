8f4223910838810ff46d6a8fc97a043756fbf54f
Gary Bethea <betheagary@gmail.com>
2015-08-23 19:46:37 -0400

    add image share funcitonality
    Signed-off-by: Gary Bethea <betheagary@gmail.com>

--- app/src/main/java/com/andoid/garebear/omgandroid/DetailActivity.java
+++ app/src/main/java/com/andoid/garebear/omgandroid/DetailActivity.java
@@ -1,8 +1,13 @@
 package com.andoid.garebear.omgandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -14,6 +19,7 @@ public class DetailActivity extends AppCompatActivity {
 
     private static final String IMAGE_URL_BASE = "http://covers.openlibrary.org/b/id/"; // 13
     String mImageURL; // 13
+    ShareActionProvider mShareActionProvider; // 14
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,5 +46,41 @@ public class DetailActivity extends AppCompatActivity {
             // Use Picasso to load the image
             Picasso.with(this).load(mImageURL).placeholder(R.mipmap.img_books_loading).into(imageView);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu
+        // this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Access the Share Item defined in menu XML
+        MenuItem shareItem = menu.findItem(R.id.menu_item_share);
+
+        // Access the object responsible for
+        // putting together the sharing submenu
+        if (shareItem != null) {
+            mShareActionProvider
+                    = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+        }
+
+        setShareIntent();
+
+        return true;
+    }
+
+    private void setShareIntent() {
+
+        // create an Intent with the contents of the TextView
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT,
+                "Book Recommendation!");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mImageURL);
+
+        // Make sure the provider knows
+        // it should work with that Intent
+        mShareActionProvider.setShareIntent(shareIntent);
     }
 }
